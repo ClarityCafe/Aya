5078872fae9874ffa4d5bea1ee45e0a009ed45f6
Ayane Satomi <chinodesuuu@gmail.com>
2020-04-23 20:04:37 +0800

    format flakeID to return a JS integer
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

--- lib/idgen.ts
+++ lib/idgen.ts
@@ -2,12 +2,15 @@
  * ID Generator using Twitter snowflakes.
  */
 import FlakeId from 'flake-idgen';
+import format from 'biguint-format';
 
-
-export default function idGen () {
+/**
+ * Generates a IEEE 754-compliant Snowflake converted to decimal. 
+ */
+export default function idGen (): Number {
     // to keep our functions in the same page, Datacenter is ID 0 and Worker is ID 1.
     // Any deviation can be a bit disastrous since we can have ID mismatches.
     const flakeId = new FlakeId({datacenter: 0, worker: 1});
 
-    return flakeId.next();
+    return format(flakeId.next(), "dec");
 }
