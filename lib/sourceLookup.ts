985698a9240c05f30c98557fad14f40e2bae1fc9
Ayane Satomi <chinodesuuu@gmail.com>
2020-04-30 11:40:27 +0800

    Compensate if Redis DB URL does not exist
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

--- lib/sourceLookup.ts
+++ lib/sourceLookup.ts
@@ -10,16 +10,19 @@ const cache = new Redis(process.env.REDIS_URL);
  * @param sourceUrl the URL for the image to look up.
  */
 export default async function sourceLookup(sourceUrl: string) {
-  try {
-    // Let's try to see if we already looked it up before...
-    return await cache.get(sourceUrl);
-  } catch {
-    // Looks like we don't know this waifu so let's try to look it up.
-    // then store it in the cache for 56 hours to lessen the load.
-    const sagiriRes: SagiriResult[] = await client(sourceUrl);
-    await cache.set(sourceUrl, sagiriRes, "ex", 1000 * 60 * 60 * 56);
+  if (process.env.REDIS_URL)
+    try {
+      // Let's try to see if we already looked it up before...
+      return await cache.get(sourceUrl);
+    } catch {
+      // Looks like we don't know this waifu so let's try to look it up.
+      // then store it in the cache for 56 hours to lessen the load.
+      const sagiriRes: SagiriResult[] = await client(sourceUrl);
+      await cache.set(sourceUrl, sagiriRes, "ex", 1000 * 60 * 60 * 56);
 
-    // return from the cache
-    return await cache.get(sourceUrl);
-  }
+      // return from the cache
+      return await cache.get(sourceUrl);
+    }
+  // don't have a Redis cache? don't worry, just return the results!
+  else return client(sourceUrl);
 }
