cef69d2025da7b051accba942f577ac6f093faf0
Ovyerus <iamovyerus@gmail.com>
2020-04-30 16:01:51 +1000

    Update body when validating request

--- lib/validate.ts
+++ lib/validate.ts
@@ -12,10 +12,11 @@ const validate = (
   { schema, location = "body" }: ValidateOptions,
   handler: NextApiHandler
 ): NextApiHandler => (req, res) => {
-  const { error } = schema.validate(req[location]);
+  const { error, value } = schema.validate(req[location]);
+  req.body = value; // Joi changes some values depending on schema, set that as body
 
   if (!error) return handler(req, res);
-  else res.status(400).json({ code: 400, message: error });
+  else res.status(400).json({ code: 400, message: error.message });
 };
 
 export default validate;
