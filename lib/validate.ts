fb9c9f000db8d7cb81410f33046fae6bf3eb4247
Ovyerus <iamovyerus@gmail.com>
2020-05-07 22:33:37 +1000

    Add better typing for validate helper

--- lib/validate.ts
+++ lib/validate.ts
@@ -1,5 +1,5 @@
 import Joi from "@hapi/joi";
-import { NextApiHandler } from "next";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
 type Locations = "body" | "query";
 
@@ -8,14 +8,31 @@ interface ValidateOptions {
   location?: Locations;
 }
 
-const validate = (
+interface ValidatedNextApiRequestBody<S> extends NextApiRequest {
+  body: S;
+}
+
+interface ValidatedNextApiRequestQuery<S extends {}> extends NextApiRequest {
+  query: S;
+}
+
+type ValidatedNextApiRequest<S, L extends Locations> = L extends "body"
+  ? ValidatedNextApiRequestBody<S>
+  : ValidatedNextApiRequestQuery<S>;
+
+type ValidatedNextApiHandler<S, L extends Locations> = (
+  req: ValidatedNextApiRequest<S, L>,
+  res: NextApiResponse
+) => void;
+
+const validate = <S = any, L extends Locations = "body">(
   { schema, location = "body" }: ValidateOptions,
-  handler: NextApiHandler
+  handler: ValidatedNextApiHandler<S, L>
 ): NextApiHandler => (req, res) => {
   const { error, value } = schema.validate(req[location]);
   req.body = value; // Joi changes some values depending on schema, set that as body
 
-  if (!error) return handler(req, res);
+  if (!error) return handler(req as ValidatedNextApiRequest<S, L>, res);
   else res.status(400).json({ code: 400, message: error.message });
 };
 
