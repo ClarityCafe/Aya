c6de1e420ae217e2ab7d0b68134039e0c7d1a94e
Ovyerus <iamovyerus@gmail.com>
2020-05-09 14:18:31 +1000

    Add authorisation functions

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,19 @@
+import { JWK, JWT } from "jose";
 import { NextApiRequest } from "next";
 
-// TODO
-export const isAuthed = (req: NextApiRequest) => true;
+const secret = process.env.AYA_JWT_SECRET;
+const secretKey = JWK.asKey(secret);
+
+export const isAuthed = (req: NextApiRequest) => {
+  const token = req.headers.authorization || req.cookies.token;
+
+  try {
+    return !!verifyToken(token);
+  } catch {
+    return false;
+  }
+};
+
+export const makeToken = (id: string) => JWT.sign({ id }, secretKey);
+
+export const verifyToken = (token: string) => JWT.verify(token, secretKey);
