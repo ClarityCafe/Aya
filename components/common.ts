bf6333288bce35366978579d6f79f4c90f58395e
Ovyerus <iamovyerus@gmail.com>
2020-05-20 22:34:23 +1000

    Add "show more" button

--- components/common.ts
+++ components/common.ts
@@ -1,6 +1,7 @@
+import { css } from "@emotion/core";
 import styled from "@emotion/styled";
 
-export const EmptyButton = styled.button`
+const buttonReset = css`
   padding: 0;
   margin: 0;
   border: none;
@@ -9,4 +10,23 @@ export const EmptyButton = styled.button`
   font: inherit;
   cursor: pointer;
   appearance: none;
+`;
+
+export const EmptyButton = styled.button`
+  ${buttonReset}
+`;
+
+export const Button = styled.button`
+  ${buttonReset}
+
+  background: var(--primary);
+  color: #fff;
+  height: 48px;
+  padding: 0 2rem;
+  border-radius: 2px;
+  transition: box-shadow 0.2s ease-out;
+
+  &:hover {
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.58);
+  }
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
+import { Button } from "~/components/common";
 import ImageCard from "~/components/image-card";
 import Navbar from "~/components/navbar";
 
@@ -40,7 +41,43 @@ const Home = () => (
         count={500}
         id="1234567890"
       />
+      <ImageCard
+        src="https://images.unsplash.com/photo-1589833788675-62e5e2aa688f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=776&q=80"
+        count={500}
+        id="1234567890"
+      />
+      <ImageCard
+        src="https://images.unsplash.com/photo-1589833788675-62e5e2aa688f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=776&q=80"
+        count={500}
+        id="1234567890"
+      />
+      <ImageCard
+        src="https://images.unsplash.com/photo-1589833788675-62e5e2aa688f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=776&q=80"
+        count={500}
+        id="1234567890"
+      />
+      <ImageCard
+        src="https://images.unsplash.com/photo-1589833788675-62e5e2aa688f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=776&q=80"
+        count={500}
+        id="1234567890"
+      />
     </main>
+
+    <div
+      css={css`
+        display: flex;
+        justify-content: center;
+        margin-top: 1rem;
+      `}
+    >
+      <Button>Show More</Button>
+    </div>
+
+    <footer
+      css={css`
+        height: 2rem;
+      `}
+    />
   </>
 );
 
