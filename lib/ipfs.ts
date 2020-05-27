e499b4eab42d22b613ebbf49a5bce65f719f28d8
Ayane Satomi <chinodesuuu@gmail.com>
2020-05-27 12:17:21 +0800

    pin objects by default on IPFS
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

--- lib/ipfs.ts
+++ lib/ipfs.ts
@@ -2,17 +2,30 @@
 import bent from "bent";
 import FormData from "form-data";
 
-export default async function upload(file: Buffer, shouldPin = false) {
+export async function upload(file: Buffer) {
   const request = bent("https://ipfs.infura.io:5001", "json", "POST", 200);
   const form = new FormData();
 
   form.append("file", file);
-
+  
   const response = await request(
-    `/api/v0/add?pin=${shouldPin}`,
+    `/api/v0/add?pin=true`,
     form,
     form.getHeaders()
   );
 
   return response;
+}
+
+export async function get(hash: string) {
+   const request = bent("https://cloudflare-ipfs.com/ipfs", "GET", 200);
+   let response: any;
+
+   try {
+     response = await request(`/${hash}`);
+   } catch (e) {
+     return new Error(e);
+   }
+
+   return response;
 }
