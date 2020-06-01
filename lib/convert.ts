c2d447ef34f6a7320290df9f0a962d9df1bfb540
Ayane Satomi <chinodesuuu@gmail.com>
2020-06-01 10:01:21 +0800

    add missing deps
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

--- lib/convert.ts
+++ lib/convert.ts
@@ -1,9 +1,11 @@
+// eslint-disable-next-line import/default
 import sharp from "sharp";
 
-
 export default function convert(file: Buffer) {
-    return sharp(file).webp({
-        lossless: true,
-        reductionEffort: 6
-    }).toBuffer();
+  return sharp(file)
+    .webp({
+      lossless: true,
+      reductionEffort: 6,
+    })
+    .toBuffer();
 }
