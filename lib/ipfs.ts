ba8e02e3a1a02a88717cc7e11ffaa59bf5f5c29a
Capuccino <chinodesuuu@gmail.com>
2020-07-29 10:24:19 +0800

    Add License Headers

--- lib/ipfs.ts
+++ lib/ipfs.ts
@@ -1,4 +1,25 @@
-/* eslint-disable import/default */
+/*
+ *  Copyright (c) 2020 Ayane Satomi, Michael Mitchell, et al.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
 import bent from "bent";
 import FormData from "form-data";
 
@@ -7,7 +28,7 @@ export async function post(file: Buffer) {
   const form = new FormData();
 
   form.append("file", file);
-  
+
   const response = await request(
     `/api/v0/add?pin=true`,
     form,
@@ -18,14 +39,14 @@ export async function post(file: Buffer) {
 }
 
 export async function get(hash: string) {
-   const request = bent("https://cloudflare-ipfs.com/ipfs", "GET", 200);
-   let response: any;
+  const request = bent("https://cloudflare-ipfs.com/ipfs", "GET", 200);
+  let response: any;
 
-   try {
-     response = await request(`/${hash}`);
-   } catch (e) {
-     return new Error(e);
-   }
+  try {
+    response = await request(`/${hash}`);
+  } catch (e) {
+    return new Error(e);
+  }
 
-   return response;
+  return response;
 }
